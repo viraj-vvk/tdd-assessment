ee3290b7bb70b59f4233c536de8441705f658c70
viraj-vvk <viraj.vvk@gmail.com>
2024-12-17 17:46:12 +0530

    Fixed test case for negative values

--- src/main/java/org/tdd/assessment/StringCalculator.java
+++ src/main/java/org/tdd/assessment/StringCalculator.java
@@ -1,9 +1,12 @@
 package org.tdd.assessment;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
     public static long add(String str) {
@@ -16,6 +19,11 @@ public class StringCalculator {
             delimiter = Pattern.quote(matcher.group(1));
             str = matcher.group(2);
         }
-        return Arrays.stream(str.split(delimiter)).map(Long::parseLong).reduce(0L, Long::sum);
+        Map<Boolean, List<Long>> filteredValues = Arrays.stream(str.split(delimiter)).map(Long::parseLong).collect(Collectors.groupingBy(val -> val < 0));
+        List<Long> negativeValues = filteredValues.get(true);
+        if (Objects.nonNull(negativeValues)) {
+            throw new RuntimeException(String.format("negative numbers not allowed : %s", negativeValues.stream().map(String::valueOf).collect(Collectors.joining(","))));
+        }
+        return filteredValues.get(false).stream().reduce(0L, Long::sum);
     }
 }
