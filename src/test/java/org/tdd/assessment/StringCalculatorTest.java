75ba07c9a7c773eb68fa6593baf1d854ab806db5
viraj-vvk <viraj.vvk@gmail.com>
2024-12-17 17:12:28 +0530

    Added failing test case for a single value String

--- src/test/java/org/tdd/assessment/StringCalculatorTest.java
+++ src/test/java/org/tdd/assessment/StringCalculatorTest.java
@@ -10,4 +10,9 @@ public class StringCalculatorTest {
         assertEquals("Test failed while sending empty string and expecting 0", 0, StringCalculator.add(""));
     }
 
+    @Test
+    public void test_add_whenInput_singleIntValue_thenOutput_sameValue() {
+        assertEquals("Test failed while sending single number and expecting same value", 1, StringCalculator.add("1"));
+    }
+
 }
