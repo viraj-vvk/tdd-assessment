ca925569153d50beb684f6424c430ceb6aba6a02
viraj-vvk <viraj.vvk@gmail.com>
2024-12-17 17:15:25 +0530

    Fixed test case for two comma separated integer value String

--- src/main/java/org/tdd/assessment/StringCalculator.java
+++ src/main/java/org/tdd/assessment/StringCalculator.java
@@ -4,6 +4,10 @@ import java.util.Objects;
 
 public class StringCalculator {
     public static long add(String str) {
-        return Objects.isNull(str) || str.isEmpty() ? 0 : Long.parseLong(str);
+        if (Objects.isNull(str) || str.isEmpty()) {
+            return 0;
+        }
+        String[] numbers = str.split(",");
+        return numbers.length > 1 ? Long.parseLong(numbers[0]) + Long.parseLong(numbers[1]) : Long.parseLong(numbers[0]);
     }
 }
