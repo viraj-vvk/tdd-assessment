d124bd9e2dbe23d378740cc4f675384a596065bd
viraj-vvk <viraj.vvk@gmail.com>
2024-12-17 17:13:08 +0530

    Fixed test case for a single value String

--- src/main/java/org/tdd/assessment/StringCalculator.java
+++ src/main/java/org/tdd/assessment/StringCalculator.java
@@ -4,6 +4,6 @@ import java.util.Objects;
 
 public class StringCalculator {
     public static long add(String str) {
-        return Objects.isNull(str) || str.isEmpty() ? 0 : Long.MIN_VALUE;
+        return Objects.isNull(str) || str.isEmpty() ? 0 : Long.parseLong(str);
     }
 }
