7878aabb35e7c23ee6510c123f467ec96a942f69
viraj-vvk <viraj.vvk@gmail.com>
2024-12-17 17:20:56 +0530

    Added failing test case for \n as delimiter

--- src/test/java/org/tdd/assessment/StringCalculatorTest.java
+++ src/test/java/org/tdd/assessment/StringCalculatorTest.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
+
+    private static final String NUMBERS = "6,5,1,2,3,20,74,108,529,639,5210,74108,52963,321079,654123,7412398,5478962";
+    private static final int SUM = 14_000_230;
+
     @Test
     public void test_add_whenInput_emptyString_thenOutput_zero() {
         assertEquals("Test failed while sending empty string and expecting 0", 0, StringCalculator.add(""));
@@ -22,7 +26,12 @@ public class StringCalculatorTest {
 
     @Test
     public void test_add_whenInput_multipleNumbersCommaSeparated_thenOutput_sum() {
-        assertEquals("Test failed while sending multiple comma separated numbers and expecting sum", 14_000_230, StringCalculator.add("6,5,1,2,3,20,74,108,529,639,5210,74108,52963,321079,654123,7412398,5478962"));
+        assertEquals("Test failed while sending multiple comma separated numbers and expecting sum", SUM, StringCalculator.add(NUMBERS));
+    }
+
+    @Test
+    public void test_add_whenInput_multipleNumbersWithNewLineAndCommaSeparated_thenOutput_sum() {
+        assertEquals("Test failed while sending ',' and '\\n' delimited numbers and expecting sum", SUM * 2, StringCalculator.add(String.format("%s\n%s", NUMBERS, NUMBERS.replaceAll(",", "\n"))));
     }
 
 }
