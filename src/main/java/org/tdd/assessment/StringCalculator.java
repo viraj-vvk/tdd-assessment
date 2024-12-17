dd415293f83bcedbfb551fc855ed13253ddf3e3c
viraj-vvk <viraj.vvk@gmail.com>
2024-12-17 17:24:54 +0530

    Fixed test case for custom delimiter

--- src/main/java/org/tdd/assessment/StringCalculator.java
+++ src/main/java/org/tdd/assessment/StringCalculator.java
@@ -2,12 +2,20 @@ package org.tdd.assessment;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
     public static long add(String str) {
         if (Objects.isNull(str) || str.isEmpty()) {
             return 0;
         }
-        return Arrays.stream(str.split(",|\n")).map(Long::parseLong).reduce(0L, Long::sum);
+        String delimiter = ",|\n";
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(str);
+        if (matcher.matches()) {
+            delimiter = matcher.group(1);
+            str = matcher.group(2);
+        }
+        return Arrays.stream(str.split(delimiter)).map(Long::parseLong).reduce(0L, Long::sum);
     }
 }
